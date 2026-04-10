c8c3221c139bc1e244bb10a6754eab9dde986c10
COMRADE040 <rafiulislam040@gmail.com>
2026-04-10 22:36:31 +0600

    finally solved

--- threeequalelements.js
+++ threeequalelements.js
@@ -12,7 +12,14 @@ var minimumDistance = function(nums) {
     let minDist = Infinity;
 
     // Seconf: check triples
-    
+    for (let indices of map.values()) {
+        if (indices.length >= 3) {
+            for (let i = 0; i <= indices.length - 3; i++) {
+                let dist = 2 * (indices[i + 2] - indices[i]);
+                minDist = Math.min(minDist, dist);
+            }
+        }
+    }
 
     return minDist === Infinity ? -1 : minDist;
 };
