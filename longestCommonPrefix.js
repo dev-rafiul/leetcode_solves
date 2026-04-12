f7b597fdb351802717cbcac59663c57c76f37735
COMRADE040 <rafiulislam040@gmail.com>
2026-04-12 23:55:25 +0600

    init for loop and also while

--- longestCommonPrefix.js
+++ longestCommonPrefix.js
@@ -3,6 +3,15 @@ var longestCommonPrefix = function(strs) {
 
     let prefix = strs[0];
 
+     
+
+    for (let i = 1; i < strs.length; i++) {
+        while (!strs[i].startsWith(prefix)) {
+            prefix = prefix.slice(0, -1); // remove last char
+            if (prefix === "") return "";
+        }
+    }
+
     
 
     return prefix;
