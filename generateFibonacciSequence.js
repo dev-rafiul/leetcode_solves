8594e5862a3e087a045880e2b8bc1cdef0f34210
COMRADE040 <rafiulislam040@gmail.com>
2026-04-16 23:55:30 +0600

    updated to comment

--- generateFibonacciSequence.js
+++ generateFibonacciSequence.js
@@ -2,10 +2,11 @@ let fibGenerator = function*(){
     let a = 0
     let b = 1;
 
-    
+    // loop condition initially true
     while(true){
         yield a;
         [a, b] = [b, a + b]
     }
+    
 
 }
